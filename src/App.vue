4ebc011a852bf7156dfdf3a98ed0db035d8c5a13
Elvis Carrasco <elvissamir18@gmail.com>
2022-01-14 16:27:18 -0400

    Added tailwind css, using vue-cli-plugin-tailwind.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1 class="hola">Hola Mundo</h1>
+  <div class="app-container">
+    <Nav></Nav>
+    <div class="content-container bg-blue-800 text-white text-5xl">
+      Content Container
+    </div>
+    <Footer></Footer>
+  </div>
 </template>
+
+<script>
+import Nav from './components/Nav.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    Nav,
+    Footer
+  },
+  setup () {
+
+  }
+}
+</script>
